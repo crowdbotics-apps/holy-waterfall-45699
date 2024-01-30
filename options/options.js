7156209e59078423293a34c4886caa7333492d12
Crowdbotics <team@crowdbotics.com>
2024-01-30 15:57:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-waterfall-45699",
+  url: "https://holy-waterfall-45699.botics.co",
+  api: "https://holy-waterfall-45699.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
